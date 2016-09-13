da6abcf5fdf3fdee58c5faea9ff2b90521501f0e
nurkumalabudif <nurkumala_fajrin_24rpl@student.smktelkom-mlg.sch.id>
2016-09-13 15:38:35 +0700

    Akhirnya bagian checkbox yang memiliki data syarat registrasi telah bisa di outputkan berupa jumlah yang talah ter "checked" oleh inputan user sendiri. Setelah itu, aplikasi android pun sudah jadi dan siap untuk digunakan.

--- app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl5024/malabox/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl5024/malabox/MainActivity.java
@@ -2,24 +2,28 @@ package id.sch.smktelkom_mlg.tugas01.xirpl5024.malabox;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener, View.OnGenericMotionListener {
 
     EditText etNamaPengirim;
-    TextView tvHasil;
+    TextView tvHasil, tvSyarat;
     Button bv;
     RadioGroup rgKR;
     RadioButton rbPn, rbPo;
     Spinner spKB;
-    CheckBox kp, kb, id;
+    CheckBox kp, kb, cbid;
+    int Syarat;
 
 
     @Override
@@ -31,12 +35,17 @@ public class MainActivity extends AppCompatActivity {
         rgKR = (RadioGroup) findViewById(R.id.radioGroupKriteria);
         /*rbPn = (RadioButton) findViewById(R.id.radioButtonPerusahaanerusahaan);
         rbPo = (RadioButton) findViewById(R.id.radioButtonPeroranganerorangan);*/
-        id = (CheckBox) findViewById(R.id.checkBoxIdentitas);
+        cbid = (CheckBox) findViewById(R.id.checkBoxIdentitas);
         kp = (CheckBox) findViewById(R.id.checkBoxPengirim);
         kb = (CheckBox) findViewById(R.id.checkBoxBarang);
         tvHasil = (TextView) findViewById(R.id.textViewHasil);
         spKB = (Spinner) findViewById(R.id.spinnerKB);
         bv = (Button) findViewById(R.id.buttonValidasi);
+        tvSyarat = (TextView) findViewById(R.id.textViewSyarat);
+
+        cbid.setOnCheckedChangeListener(this);
+        kp.setOnGenericMotionListener(this);
+        kb.setOnCheckedChangeListener(this);
 
 
         bv.setOnClickListener(new View.OnClickListener() {
@@ -49,16 +58,25 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-            private void doClick() {
-                if (isValid()) {
-                    String nama = etNamaPengirim.getText().toString();
+    private void doClick() {
 
-                    String hasil = null;
-                    if (rgKR.getCheckedRadioButtonId() != -1) {
-                        RadioButton rb = (RadioButton)
-                                findViewById(rgKR.getCheckedRadioButtonId());
-                        hasil = rb.getText().toString();
-                    }
+        String syarat = "Jumlah syarat Ketentutan yang telah anda validasi adalah :\n ";
+        int startlen = syarat.length();
+        if (cbid.isChecked()) syarat += cbid.getText() + "\n";
+        if (kp.isChecked()) syarat += kp.getText() + "\n";
+        if (kb.isChecked()) syarat += kb.getText() + "\n";
+
+        if (syarat.length() == startlen) syarat += "Tidak Ada pada Pilihan";
+
+        if (isValid()) {
+            String nama = etNamaPengirim.getText().toString();
+
+            String hasil = null;
+            if (rgKR.getCheckedRadioButtonId() != -1) {
+                RadioButton rb = (RadioButton)
+                        findViewById(rgKR.getCheckedRadioButtonId());
+                hasil = rb.getText().toString();
+            }
                     /*if(rbPn.isChecked())
                     {
                         hasil = rbPn.getText().toString();
@@ -70,9 +88,9 @@ public class MainActivity extends AppCompatActivity {
                     else if(rbPo.isChecked()){
                         hasil = rbPo.getText().toString();
                     }*/
-                    tvHasil.setText("Nama Anda : " + nama + "\n" + "Kriteria Pengirim : " + hasil + "\n" + "Kriteria Barang : " + spKB.getSelectedItem().toString());
-                }
-            }
+            tvHasil.setText("Nama Anda : " + nama + "\n" + "Kriteria Pengirim : " + hasil + "\n" + "Kriteria Barang : " + spKB.getSelectedItem().toString());
+        }
+    }
 
 
     private void doProcess() {
@@ -96,5 +114,18 @@ public class MainActivity extends AppCompatActivity {
             etNamaPengirim.setError(null);
         }
         return valid;
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        if (isChecked) Syarat += 1;
+        else Syarat -= 2;
+
+        tvSyarat.setText("Jumlah syarat yang dikumpulkan adalah (" + Syarat + " terpilih)");
+    }
+
+    @Override
+    public boolean onGenericMotion(View v, MotionEvent event) {
+        return false;
     }
 }
